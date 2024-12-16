fc72fe4e430ef27bbb25cbc99f68c90391caec75
Sergiu <puteresergiu10@gmail.com>
2024-12-16 17:56:05 +0200

    TPA(sper)

---
+++ TPA_lectii/TPA_lectii/Banc_Accounts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPA_lectii
+{
+        public class Accaunts
+        {
+            public string Name;
+            public int Number;
+            public int Sum;
+            public string Pin;
+            public int Date;
+
+            public Accaunts(string name, int number, int sum, string pin)
+            {
+                Name = name;
+                Number = number;
+                Sum = sum;
+                Pin = pin;
+                Date = DateTime.UtcNow.Year;
+            }
+            public void Sold()
+            {
+                Console.WriteLine($"In acaunt sunt {Sum} bani");
+            }
+            public void Alimentare(int sum)
+            {
+                Sum += sum;
+                Console.WriteLine("Alimentare efectuata cu succes");
+            }
+
+            public void Exstragere(int sum)
+            {
+                if (Sum - sum >= 0)
+                {
+                    Sum -= sum;
+                    Console.WriteLine("Extragere efectuata cu succes");
+                }
+                else
+                {
+                    Console.WriteLine("Insuficent funds");
+                }
+            }
+        }
+}
---
+++ TPA_lectii/TPA_lectii/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TPA_lectii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angajat angajat = new Angajat("Vasile","c",421);
+            angajat.Afisare();
+            Accaunts acaunt = new Accaunts("Andrei",142531,10,"afwafregw");
+            acaunt.Sold();
+            acaunt.Alimentare(3);
+            acaunt.Sold();        
+            acaunt.Exstragere(12);
+            acaunt.Sold();
+            acaunt.Exstragere(4);
+            acaunt.Sold();
+        }
+    }
+}
