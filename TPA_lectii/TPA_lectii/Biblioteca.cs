6fcbb21cf65fa2997a3119ce9617cf141fde9cb1
Sergiu <puteresergiu10@gmail.com>
2025-01-12 20:28:40 +0200

    TPA7

---
+++ TPA_lectii/TPA_lectii/Biblioteca.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPA_lectii
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public int CopiesAvailable { get; set; }
+
+        
+    }
+
+    public static class LINQ
+    {
+        public static List<Book> FindTroelsen(List<Book> books)
+        {
+            var bookByTroelsen = from book in books
+                                 where book.Author == "Andrew Troelsen"
+                                 select book;
+            return bookByTroelsen.ToList();
+        }
+        public static List<Book> Ordonare(List<Book> books)
+        {
+            var orderByYear = books.OrderBy(book => book.Year);
+            return orderByYear.ToList();
+        }
+        public static List<Book> Proectie(List<Book> books)
+        {
+            var copieDisponibila = from book in books
+                                   where book.CopiesAvailable >= 1
+                                   select book;
+            return copieDisponibila.ToList();
+        }
+        public static int Agregare(List<Book> books)
+        {
+            var NrCopiiTotal = books
+                .Select(a => a.CopiesAvailable)
+                .Sum();
+            return NrCopiiTotal;
+        }
+        public static List<string> Distinct(List<Book> books)
+        {
+            var uniqueAuthors = books
+            .Select(b => b.Author)
+            .Distinct()
+            .ToList();
+            return uniqueAuthors.ToList();
+        }
+        public static List<Book> Pagini(List<Book> books)
+        {
+            var ADouaPagina = books.Skip(2).Take(2);
+            return ADouaPagina.ToList();
+        }
+    }
+
+}
--- TPA_lectii/TPA_lectii/Program.cs
+++ TPA_lectii/TPA_lectii/Program.cs
@@ -4,28 +4,42 @@
     {
         static void Main(string[] args)
         {
-            var stringObject = new PriorityQueue<string>();
-            Console.WriteLine(stringObject.IsEmpty());
-            stringObject.Enqueue("Dinosaur",14);
-            stringObject.Enqueue("Delfin", 15);
-            stringObject.Enqueue("Elefnat", 12);
-            Console.WriteLine(stringObject.IsEmpty());
-            Console.WriteLine(stringObject.Peek());
-            Console.WriteLine(stringObject.Dequeue());
-            Console.WriteLine(stringObject.Peek());
+            List<Book> books = new List<Book>
+            {
+            new Book { Id = 1, Title = "C# in Depth", Author = "Jon Skeet", Year = 2019, CopiesAvailable = 5 },
+            new Book { Id = 2, Title = "Pro C# 7", Author = "Andrew Troelsen", Year = 2018, CopiesAvailable = 2 },
+            new Book { Id = 3, Title = "C# 6.0 and the .NET 4.6 Framework", Author = "Andrew Troelsen", Year = 2015, CopiesAvailable = 0 },
+            new Book { Id = 4, Title = "Learning C# by Developing Games", Author = "Harrison Ferrone", Year = 2020, CopiesAvailable = 4 },
+            new Book { Id = 5, Title = "CLR via C#", Author = "Jeffrey Richter", Year = 2012, CopiesAvailable = 1 }
+            };
+            var first = LINQ.FindTroelsen(books);
+            foreach (var book in first)
+                Console.WriteLine(book.Id);
             Console.WriteLine();
 
-            var intObject = new PriorityQueue<int>();
-            Console.WriteLine(intObject.IsEmpty());
-            intObject.Enqueue(1, -10);
-            intObject.Enqueue(2, -2);
-            intObject.Enqueue(3, 401);
-            Console.WriteLine(intObject.Peek());
-            Console.WriteLine(intObject.Dequeue());
-            Console.WriteLine(intObject.Peek());
+            var second = LINQ.Ordonare(books);
+            foreach (var book in second)
+                Console.WriteLine(book.Id);
             Console.WriteLine();
 
+            var third = LINQ.Proectie(books);
+            foreach (var book in third)
+                Console.WriteLine(book.Id);
+            Console.WriteLine();
 
+            
+            Console.WriteLine(LINQ.Agregare(books));
+            Console.WriteLine();
+
+            var forth = LINQ.Proectie(books);
+            foreach (var book in forth)
+                Console.WriteLine(book.Id);
+            Console.WriteLine();
+
+            var fifth = LINQ.Pagini(books);
+            foreach (var book in fifth)
+                Console.WriteLine(book.Id);
+            Console.WriteLine();
 
 
         }
