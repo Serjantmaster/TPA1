acadc281a0fedef96a4b3b461efdfb5a15998616
Sergiu <puteresergiu10@gmail.com>
2025-01-21 17:57:01 +0200

    TPA11

---
+++ TPA_lectii/TestProject/ShoppingCartTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using TPA_lectii;
+
+namespace TestProject
+{
+    public class ShoppingCartTest
+    {
+        [Fact]
+        public void GetProductById_ShouldBeUsedOnce()
+        {
+            var productRepositoryMock = new Mock<IProductRepository>();
+            var shoppingCart = new ShoppingCart(productRepositoryMock.Object);
+            var productId = 1;
+            shoppingCart.AddToCart(productId);
+            productRepositoryMock.Verify(p => p.GetProductById(productId),Times.Once);
+        }
+        [Fact]
+        public void CalculateTotal_ShouldReturnSumOfItem()
+        {
+            var productRepositoryMock = new Mock<IProductRepository>();
+            var shopingCart = new ShoppingCart(productRepositoryMock.Object);
+            var product = new Product
+            {
+                Price = 12345,
+            };
+            var productId = 1;
+            productRepositoryMock.Setup(p => p.GetProductById(productId)).Returns(product);
+
+            shopingCart.AddToCart(productId);
+            var result = shopingCart.CalculateTotal();
+
+            Assert.Equal(12345, result);
+
+        }
+    }
+}
